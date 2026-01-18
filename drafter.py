9956a18b284f22c6d13d2d4f18ea70b491b62edf
Gustavo Menezes <menezesgus@outlook.com>
2026-01-17 23:08:47 -0300

    aa

---
+++ drafter.py
@@ -0,0 +1,98 @@
+from typing import Annotated, Sequence, TypedDict
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage, ToolMessage, BaseMessage
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, END, START
+from langgraph.prebuilt import ToolNode
+
+load_dotenv()
+
+document_content = ""
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+@tool
+def update(content: str) -> str:
+    """Updates the document with the provided content."""
+    global document_content
+    document_content = content
+    return f"Document has been updated sucessfully! The current content is: \n {document_content}"
+
+@tool
+def save(filename: str) -> str:
+    """Save the current document to a text file and finish the process.
+    
+    Args:
+        filename: Name for the text file.
+    """
+    global document_content
+
+    if not filename.endswith(".txt"):
+        filename = f"{filename}.txt"
+
+    try:
+        with open(filename, 'w') as file:
+            file.write(document_content)
+        print(f"\n Document has been saved to: {filename}")
+        return f"\n Document has been succesfully saved to: {filename}"
+    except Exception as e:
+        return f"Error saving document: {str(e)}"
+    
+tools = [update, save]
+
+model = ChatOpenAI(model="gpt-4o").bind_tools(tools)
+
+
+def our_agent(state: AgentState) -> AgentState:
+
+    system_prompt = SystemMessage(content=f"""
+    You are Drafter, a helpful writing assistant. You are going to help the user update and modify documents.
+    
+    - If the user wants to update or modify content, use the 'update' tool with the complete updated content.
+    - If the user wants to save and finish, you need to use the 'save' tool.
+    - Make sure to always show the current document state after modifications.
+    
+    The current document content is:{document_content}
+    """)
+
+    if not state['messages']:
+        user_input = "I'm ready to help you update a document. What would you like to create?"
+        user_message = HumanMessage(content=user_input)
+
+    else:
+        user_input = input("\nWhat would you like to do with the document?")
+        print(f"\n User: {user_input}")
+        user_message = HumanMessage(content=user_input)
+
+    all_messages = [system_prompt] + list(state["messages"]) + [user_message]
+
+    response = model.invoke(all_messages)
+
+    print(f"\n AI: {response.content}")
+    if hasattr(response, "tool_calls") and response.tool_calls:
+        print(f"USING TOOLS: {[tc['name'] for tc in response.tool_calls]}")
+
+    return {"messages": list(state["messages"]) + [user_message, response]}
+
+
+
+def should_continue(state: AgentState) -> str:
+    """Determine if we should continue or end the conversation."""
+
+    messages = state["messages"]
+    
+    if not messages:
+        return "continue"
+    
+    # This looks for the most recent tool message....
+    for message in reversed(messages):
+        # ... and checks if this is a ToolMessage resulting from save
+        if (isinstance(message, ToolMessage) and 
+            "saved" in message.content.lower() and
+            "document" in message.content.lower()):
+            return "end" # goes to the end edge which leads to the endpoint
+        
+    return "continue"
